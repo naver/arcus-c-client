63a66d4e88f4e8d4a71f678c2220bf5cd65aff8a
Park JunHyun <junhyun.park@jam2in.com>
2015-06-21 08:38:50 +0900

    Use 160 hash points per server when all servers have the same weight.

--- libmemcached/hosts.cc
+++ libmemcached/hosts.cc
@@ -261,6 +261,11 @@ static memcached_return_t update_continuum(memcached_st *ptr)
   }
 
   uint64_t total_weight= 0;
+#if 1 // CHECK_ALL_WEIGHTS_ARE_SAME
+  uint32_t total_server= 0;
+  uint32_t first_weight;
+  bool all_weights_same= true;
+#endif
   if (is_ketama_weighted)
   {
     for (uint32_t host_index = 0; host_index < memcached_server_count(ptr); ++host_index)
@@ -268,6 +273,15 @@ static memcached_return_t update_continuum(memcached_st *ptr)
       if (is_auto_ejecting == false or list[host_index].next_retry <= now.tv_sec)
       {
         total_weight += list[host_index].weight;
+#if 1 // CHECK_ALL_WEIGHTS_ARE_SAME
+        /* check if all weights are same */
+        if ((++total_server) == 1) {
+          first_weight = list[host_index].weight;
+        } else {
+          if (first_weight != list[host_index].weight)
+            all_weights_same= false;
+        }
+#endif
       }
     }
   }
@@ -281,8 +295,16 @@ static memcached_return_t update_continuum(memcached_st *ptr)
 
     if (is_ketama_weighted)
     {
-        float pct= (float)list[host_index].weight / (float)total_weight;
-        pointer_per_server= (uint32_t) ((floor((float) (pct * MEMCACHED_POINTS_PER_SERVER_KETAMA / 4 * (float)live_servers + 0.0000000001))) * 4);
+#if 1 // CHECK_ALL_WEIGHTS_ARE_SAME
+        if (all_weights_same) {
+          pointer_per_server= MEMCACHED_POINTS_PER_SERVER_KETAMA;
+        } else {
+#endif
+          float pct= (float)list[host_index].weight / (float)total_weight;
+          pointer_per_server= (uint32_t) ((floor((float) (pct * MEMCACHED_POINTS_PER_SERVER_KETAMA / 4 * (float)live_servers + 0.0000000001))) * 4);
+#if 1 // CHECK_ALL_WEIGHTS_ARE_SAME
+        }
+#endif
         pointer_per_hash= 4;
         if (DEBUG)
         {
