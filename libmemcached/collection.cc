0715226c92e7d5c6c90f7611fc68f446e8b28d9b
jangsuhwan <baseball9185@github.com>
2019-04-29 11:58:47 +0900

    FIX: Get collection attributes(type, readable)

--- libmemcached/collection.cc
+++ libmemcached/collection.cc
@@ -757,6 +757,8 @@ memcached_return_t memcached_get_attrs(memcached_st *ptr,
         }
         else if (strncmp("type", attr_name, sizeof(attr_name)) == 0)
         {
+          word[word_length - 2] = '\0'; // \r
+          word[word_length - 1] = '\0'; // \n
           attrs->type = str_to_type(word, strlen(word));
         }
         else if (strncmp("count", attr_name, sizeof(attr_name)) == 0)
@@ -772,6 +774,10 @@ memcached_return_t memcached_get_attrs(memcached_st *ptr,
           word[word_length - 2] = '\0'; // \r
           word[word_length - 1] = '\0'; // \n
           attrs->overflowaction = str_to_overflowaction(word, strlen(word));
+        }
+        else if (strncmp("readable", attr_name, sizeof(attr_name)) == 0)
+        {
+          attrs->readable = (word[0] == 'o' && word[1] == 'n');
         }
         else if (strncmp("minbkey", attr_name, sizeof(attr_name)) == 0)
         {
