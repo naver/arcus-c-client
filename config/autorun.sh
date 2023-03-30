d8ce5f329e300c7f7c4e0661c819e1e1bb5f948f
namsic <kimnj3050@gmail.com>
2023-03-30 17:18:09 +0900

    INTERNAL: Do not use UNKNOWN version

--- config/autorun.sh
+++ config/autorun.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 if [[ -d .git || -f .git || ! -f m4/version.m4 ]]
 then
-  perl config/version.pl || die "Failed to run config/version.pl"
+  perl config/version.pl
 fi
 
 autoreconf --install --force --verbose -Wall
